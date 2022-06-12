0ae53b63322cbb69be5f59f79fec67d8ab94c471
gabrielreis35 <gabrielreis@vixdynamics.com.br>
2022-06-12 17:07:41 -0300

    Adição da repetição

--- Program.cs
+++ Program.cs
@@ -3,25 +3,42 @@ using System;
 namespace textEditor {
     class Program {
         static void Main(string[] args){
+            bool verify_todo = False;
             int todo = 0;
         
             StreamReader reader = new StreamReader("br-dictionary.txt");
             string data = reader.ReadToEnd();
             string[] words = data.Split("\n");
 
-            Console.WriteLine(words[]);
-
             Console.WriteLine("O que deseja fazer?");
             Console.WriteLine("1 - Adicionar nova palavra.");
             Console.WriteLine("2 - Verificar texto.");
             Console.ReadLine(todo);
-
-            if (todo == 1){
-                // Executar a função de adicionar nova palavra no dicionário;
-            }
-            else if (todo == 2){
-                // Executar a função de verificar o texto;
-            }
+            
+            do{    
+                if (todo == 1){
+                    // Executar a função de adicionar nova palavra no dicionário;
+                    verify_todo = True;
+                }
+                else if (todo == 2){
+                    // Executar a função de verificar o texto;
+                    verify_todo = True;
+                }
+                else {
+                    // Resposta não compreendida;
+                    verify_todo = False;
+                    Console.WriteLine("Resposta não válida, por favor informar novamente!");
+                    Console.WriteLine("1 - Adicionar nova palavra.");
+                    Console.WriteLine("2 - Verificar texto.");
+                    Console.ReadLine(todo);
+                }
+            } while (verify_todo == False);
         }
     }
 }
+
+
+// use link -> ".contains" = "contem tal palavra?"
+// rejecs
+
+// fazer o texto da var e o split do dicionário (array) -> texto.contains(dicionário.split())
