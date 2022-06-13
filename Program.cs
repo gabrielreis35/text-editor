1b79b3d660b586421548bd74817b60fd4596dcbc
gabrielreis35 <gabrielreis@vixdynamics.com.br>
2022-06-13 01:54:41 -0300

    Criação do projeto a adição da explicação no ReadMe

--- Program.cs
+++ Program.cs
@@ -1,54 +1,55 @@
-using System;
+﻿using System;
 
 namespace textEditor {
     class Program {
-        string Dictionary(){
+        static string ReadFile(){
             // Inicializando o dicionário com as palavras padrões;
-            StreamReader reader = new StreamReader("br-dictionary.txt");
-            string data = reader.ReadToEnd();
-            string[] words = data.Split("\n");
-            return(words);
+            return("0");
         }
         string AddWord(string newWord){
             // Adicionando palavra ao dicionário;
+            return ("0");
         }
 
         static void Main(string[] args){
             // Iniciando as variáveis globais;
-            bool verify_todo = False;
+            bool verify_todo = false;
             int todo = 0;
             string newWord;
-            string[] dict;
+            //string[] dictionary;
 
-            dict = Dictionary();
-            
+            StreamReader reader = new StreamReader("br-dictionary.txt");
+            string data = reader.ReadToEnd();
+            string[] words = data.Split("\n");
+            // dictionary = ReadFile();
+
             // Iniciando o Programa;
             Console.WriteLine("O que deseja fazer?");
             Console.WriteLine("1 - Adicionar nova palavra.");
             Console.WriteLine("2 - Verificar texto.");
-            Console.ReadLine(todo);
+            todo = Convert.ToInt32(Console.ReadLine());
             
             do{    
                 if (todo == 1){
                     // Executar a função de adicionar nova palavra no dicionário;
-                    verify_todo = True;
-                    Console.WriteLine("Qual palavra deseja adicionar?");
-                    Console.ReadLine(newWord);
-                    AddWord(newWord);
+                    verify_todo = true;
+                    Console.WriteLine("Qual palavra deseja adicionar? ");
+                    newWord = Console.ReadLine();
+                    Console.WriteLine("Palavra adicionada - " + newWord);
                 }
                 else if (todo == 2){
                     // Executar a função de verificar o texto;
-                    verify_todo = True;
+                    verify_todo = true;
                 }
                 else {
                     // Resposta não compreendida;
-                    verify_todo = False;
+                    verify_todo = false;
                     Console.WriteLine("Resposta não válida, por favor informar novamente!");
                     Console.WriteLine("1 - Adicionar nova palavra.");
                     Console.WriteLine("2 - Verificar texto.");
-                    Console.ReadLine(todo);
+                    todo = Convert.ToInt32(Console.ReadLine());
                 }
-            } while (verify_todo == False);
+            } while (verify_todo == false);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # text-editor
+
+Funcionamento do programa:
+
+    1 - O projeto começa com a leitura de um arquivo .txt com uma base de palavras já pré definidas, adicionando numa tabela Hash.
+
+    2 - Assim que terminar de fazer a leitura, o programa vai perguntar se o usuário quer adicionar uma palavra nova ou verificar o texto.
+
+    3 - O programa vai pegar um arquivo .txt onde tem o texto escrito, vai colocar em uma variável, quebrar o texto por palavras e verificar 1 por 1.
+
+    4 - Assim que terminar de verificar ele vai entregar um novo txt com o texto original e com as palavras não existentes no "banco de dados", ou seja, na tabela Hash.
+
+Estrutura:
+
+    1 - Para salvar as palavras e fazer a verificação será salvo uma lista de tabelas Hash, contendo 27 tabelas, cada uma representando 1 letra do alfabeto. A palavra vai ser salva dentro da tabela conforme a tua primeira letra, assim podemos fazer uma divisão melhor para a verificação de cada palavra. O modo de tratamento de colisão foi feito por modo estruturado, ou seja, apontando caminhos dentro de cada chave, assim podendo ter mais de 1 variação dentro da mesma posição, e pensando nisso, o modo de divisão da tabela será por número de letras por palavra, deixando mais rápido de encontrar a palavra verificada.
+
+    2 - O projeto foi divido em funções para a manipulação e a limpeza do código, para ficar de fácil entendimento.
