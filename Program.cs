3e046ff70d0dcc7142cf301986ffdcb5229acb8a
gabrielreis35 <gabrielreis@vixdynamics.com.br>
2022-06-12 22:38:49 -0300

    Criando as funções separads

--- Program.cs
+++ Program.cs
@@ -2,14 +2,23 @@ using System;
 
 namespace textEditor {
     class Program {
-        static void Main(string[] args){
-            bool verify_todo = False;
-            int todo = 0;
-        
+        string Dictionary(){
+            // Inicializando o dicionário com as palavras padrões;
             StreamReader reader = new StreamReader("br-dictionary.txt");
             string data = reader.ReadToEnd();
             string[] words = data.Split("\n");
+        }
+        string AddWord(string newWord){
+            // Adicionando palavra ao dicionário;
+        }
 
+        static void Main(string[] args){
+            // Iniciando as variáveis globais;
+            bool verify_todo = False;
+            int todo = 0;
+            string newWord;
+
+            // Iniciando o Programa;
             Console.WriteLine("O que deseja fazer?");
             Console.WriteLine("1 - Adicionar nova palavra.");
             Console.WriteLine("2 - Verificar texto.");
@@ -19,6 +28,9 @@ namespace textEditor {
                 if (todo == 1){
                     // Executar a função de adicionar nova palavra no dicionário;
                     verify_todo = True;
+                    Console.WriteLine("Qual palavra deseja adicionar?");
+                    Console.ReadLine(newWord);
+                    AddWord(newWord);
                 }
                 else if (todo == 2){
                     // Executar a função de verificar o texto;
