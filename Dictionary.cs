56d4bdd5da35ffe5ccfbbf994d589271d2d3ff1f
gabrielreis35 <gabrielreis@vixdynamics.com.br>
2022-06-15 03:02:39 -0300

    Acerto no Dictionary

--- Dictionary.cs
+++ Dictionary.cs
@@ -29,153 +29,132 @@ namespace dictionary{
             table_d.Add(words, numLetter);
         }
     }
-
     public class Hashtable_e{
         private static Hashtable table_e = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_e.Add(words, numLetter);
         }
     }
-
     public class Hashtable_f{
         private static Hashtable table_f = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_f.Add(words, numLetter);
         }
     }
-
     public class Hashtable_g{
         private static Hashtable table_g = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_g.Add(words, numLetter);
         }
     }
-
     public class Hashtable_h{
         private static Hashtable table_h = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_h.Add(words, numLetter);
         }
     }
-
     public class Hashtable_i{
         private static Hashtable table_i = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_i.Add(words, numLetter);
         }
     }
-
     public class Hashtable_j{
         private static Hashtable table_j = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_j.Add(words, numLetter);
         }
     }
-
     public class Hashtable_k{
         private static Hashtable table_k = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_k.Add(words, numLetter);
         }
     }
-
     public class Hashtable_l{
         private static Hashtable table_l = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_l.Add(words, numLetter);
         }
     }
-
     public class Hashtable_m{
         private static Hashtable table_m = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_m.Add(words, numLetter);
         }
     }
-
     public class Hashtable_n{
         private static Hashtable table_n = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_n.Add(words, numLetter);
         }
     }
-
     public class Hashtable_o{
         private static Hashtable table_o = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_o.Add(words, numLetter);
         }
     }
-
     public class Hashtable_p{
         private static Hashtable table_p = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_p.Add(words, numLetter);
         }
     }
-
     public class Hashtable_q{
         private static Hashtable table_q = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_q.Add(words, numLetter);
         }
     }
-
     public class Hashtable_r{
         private static Hashtable table_r = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_r.Add(words, numLetter);
         }
     }
-
     public class Hashtable_s{
         private static Hashtable table_s = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_s.Add(words, numLetter);
         }
     }
-
     public class Hashtable_t{
         private static Hashtable table_t = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_t.Add(words, numLetter);
         }
     }
-
     public class Hashtable_u{
         private static Hashtable table_u = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_u.Add(words, numLetter);
         }
     }
-    public class Hashtable_v{
+   public class Hashtable_v{
         private static Hashtable table_v = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_v.Add(words, numLetter);
         }
     }
-
     public class Hashtable_w{
         private static Hashtable table_w = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_w.Add(words, numLetter);
         }
     }
-
     public class Hashtable_x{
         private static Hashtable table_x = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_x.Add(words, numLetter);
         }
     }
-
     public class Hashtable_y{
         private static Hashtable table_y = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
             table_y.Add(words, numLetter);
         }
     }
-
     public class Hashtable_z{
         private static Hashtable table_z = new Hashtable(){};
         public static void AddAWords(string words, int numLetter){
